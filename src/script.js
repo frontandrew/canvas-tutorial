0bb88cef2adbf3f19bbd5e6e2924aa3def6f77dd
AndrewBubnov <jackblack.nt@gmail.com>
2023-11-24 02:29:59 +0500

    feat: remake all process

--- src/script.js
+++ src/script.js
@@ -1,32 +1,45 @@
-const ctx = document.getElementById('canvas').getContext('2d')
-
-canvas.width = 400
-canvas.height = 400
+const logoConfig = {
+    width: 400,
+    height: 400,
+    vertical: 150,
+    horizontal: 100,
+    thickness: 16,
+    color: 'white',
+    debug: true,
+}
 
 class Logo {
-    constructor(context, config) {
-        this.width = context.canvas.width
-        this.height = context.canvas.height
-        this.ctx = context
+    constructor(config) {
         this.config = config
         this.defaultColor = 'yellow'
         this.indentCoef = 22 / 16
-        this.currentOffset = { x: 0, y: 0 }
+        this.ctx = null
+        this.element = null
     }
 
-    _debug(params) {
-        console.log('DEBUG: ', { ...params })
+    init() {
+        const { width: wd, height: hg  } = this.config
+
+        this.element = document.createElement('canvas');
+        this.ctx = this.element.getContext('2d');
+        this.element.height = hg
+        this.element.width = wd
+
+        this._render()
+
+        return this.element
     }
 
-    render(dx = 0, dy = 0, color) {
-        this._debug({ ...this, params: { diff: { dx, dy, renderColor: color } } })
+    _debug() {
+        if (this.config.debug) {
+            console.log('LOGO DEBUG: ', this)
+        }
+    }
 
-        const { horizontal: hl, vertical: vl, thickness: th } = this.config
-        const {indentCoef: coef, width: wd, height: hg} = this
+    _render() {
+        const { horizontal: hl, vertical: vl, thickness: th, width: wd, height: hg  } = this.config
+        const { indentCoef: coef } = this
 
-        this._setOffset(dx, dy)
-        const { x, y } = this.currentOffset
-        
         const vert1PosX = (wd / 2) - (hl / 2) - (th * coef) - th
         const vert2PosX = (wd / 2) + (hl / 2) + (th * coef)
         const horizPosX = (wd / 2) - (hl / 2)
@@ -36,51 +49,133 @@ class Logo {
         const horixPosY = (hg / 2) - (vl / 2) - th
 
         this._clear()
-        this._setColor(color)
-        this._createLine(vert1PosX + x, vert1PosY + y, th, vl)
-        this._createLine(vert2PosX + x, vert2PosY + y, th, vl)
-        this._createLine(horizPosX + x, horixPosY + y, hl, th)
-    }
-
-    _setOffset(dx, dy) {
-        this.currentOffset.x += dx
-        this.currentOffset.y += dy        
+        this._createLine(vert1PosX, vert1PosY, th, vl)
+        this._createLine(vert2PosX, vert2PosY, th, vl)
+        this._createLine(horizPosX, horixPosY, hl, th)
+        this._setColor()
     }
 
     _createLine(x, y, width, height) {
-        this.ctx.fillRect(x, y, width, height)
+        this.ctx.rect(x, y, width, height)
     }
 
     _clear() {
         this.ctx.clearRect(0, 0, this.width, this.height)
     }
 
-    _setColor(color) {
-        this.ctx.fillStyle = color || this.config.color || this.defaultColor
+    _setColor() {
+        this.ctx.fillStyle = this.config.color || this.defaultColor
+        this.ctx.fill()
     }
 }
 
-const logoConfig = {
-    vertical: 150,
-    horizontal: 100,
-    thickness: 16,
-    color: 'white',
+class Renderer {
+    constructor(config) {
+        this.config = config
+        this.element = null
+        this.ctx = null
+        this.matrix = null
+        this.pattern = null
+    }
+
+    init() {
+        this.element = document.getElementById(this.config.id)
+        this.ctx = this.element.getContext('2d')
+
+        this.element.width = 400
+        this.element.height = 400
+
+        this.matrix = new DOMMatrix()
+        this.pattern = this.ctx.createPattern(this.config.animate, 'repeat')
+        this.pattern.setTransform(this.matrix)
+        
+        const { dx, dy } = this._calcDirrection()
+        this._render(dx, dy)
+    }
+
+    _calcDirrection(event) {
+        const code = event?.code ?? null
+        const { step } = this.config
+
+        if(code === 'ArrowUp') return { dx: 0, dy: -step }
+        if(code === 'ArrowDown') return { dx: 0, dy: step }
+        if(code === 'ArrowLeft') return { dx: -step, dy: 0 }
+        if(code === 'ArrowRight') return { dx: step, dy: 0 }
+
+        return { dx: 0, dy: 0 }
+    }
+
+    _render(dx, dy) {
+        this._clear()
+        this.matrix.translateSelf(dx, dy);
+        this.pattern.setTransform(this.matrix);
+
+        this.ctx.fillStyle = this.pattern
+        this.ctx.fillRect(0, 0, this.element.width, this.element.height)
+    }
+
+    _clear() {
+        this.ctx.clearRect(0, 0, this.element.width, this.element.height)
+    }
+
+    animate(event) {
+        const { dx, dy } = this._calcDirrection(event)
+        this._render(dx, dy)
+    }
+
 }
-const logo = new Logo(ctx, logoConfig)
 
-const STEP = 25;
+const logo = new Logo(logoConfig)
+const animatedElement = logo.init()
 
-logo.render(0, 0)
+const rendererConf = {
+    id: 'canvas',
+    animate: animatedElement,
+    step: 25,
+}
 
-document.addEventListener('keyup', function({ code }) {
-    console.log('EVENT: ', { code })
+const renderer = new Renderer(rendererConf)
+renderer.init()
 
-    const { dx, dy } = function(code = '') {
-        if(code === 'ArrowUp') return { dx: 0, dy: -STEP }
-        if(code === 'ArrowDown') return { dx: 0, dy: STEP }
-        if(code === 'ArrowLeft') return { dx: -STEP, dy: 0 }
-        if(code === 'ArrowRight') return { dx: STEP, dy: 0 }
-    }(code)
+document.addEventListener('keyup', (e) => renderer.animate(e))
 
-    logo.render(dx, dy)
-})
+/////////////////////////////////////////////////////////////
+
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+
+// canvas.width = 400
+// canvas.height = 400
+
+// const pattern = ctx.createPattern(animatedElement, 'repeat');
+// const matrix = new DOMMatrix();
+
+// pattern.setTransform(matrix);
+// ctx.fillStyle = pattern;
+// ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// document.addEventListener('keyup', function({ code }) {
+//     console.log('EVENT: ', { code })
+
+//     const { dx, dy } = function(code = '') {
+//         if(code === 'ArrowUp') return { dx: 0, dy: -STEP }
+//         if(code === 'ArrowDown') return { dx: 0, dy: STEP }
+//         if(code === 'ArrowLeft') return { dx: -STEP, dy: 0 }
+//         if(code === 'ArrowRight') return { dx: STEP, dy: 0 }
+//     }(code)
+
+//     logo.render(dx, dy)
+// })
+
+// function loop() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+//     matrix.translateSelf(0.5, 1);
+//     pattern.setTransform(matrix);
+    
+//     ctx.fillStyle = pattern;
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+//     requestAnimationFrame(loop);
+//   }
+
+//   requestAnimationFrame(loop);
